38562ac2a8945a88293850b39d08c69d2084b62a
pavithra21635 <pavithra21635@gmail.com>
2024-08-20 15:04:55 +0530

    Creating trainLocationController

---
+++ src/controllers/trainLocationController.js
@@ -0,0 +1,17 @@
+const trainLocationData = require('../models/trainLocationModel');
+
+
+async function getLastTwoTrainLocations(req, res) {
+    try {
+        // Retrieve the last two entries, sorted by timestamp in descending order
+        const latestTrainLocations = await trainLocations.find().sort({ timestamp: -1 }).limit(2);
+        
+        // Send the retrieved data as a response
+        res.status(200).json(latestTrainLocations);
+    } catch (error) {
+        console.error('Error retrieving train location data:', error);
+        res.status(500).json({ error: 'Error retrieving data' });
+    }
+}
+
+module.exports = getLastTwoTrainLocations;
